1c5fad855efb7fb547f10bac011d3edda3e8cb10
Andrew Munro <admunro@gmx.de>
2023-12-10 01:03:11 +0100

    Day 9 solution. Yuck.

---
+++ Day 9/day9part2.py
@@ -0,0 +1,51 @@
+def get_input(filename):
+    with open(filename) as inputFile:
+        raw_input = [(line.strip()) for line in inputFile]
+
+    parsed_input = []
+
+    for line in raw_input:
+        parsed_input.append([int(value) for value in line.split()])
+
+    return parsed_input
+
+
+def reduce(input):
+
+    results = [input]
+
+    while True:
+
+        differences = []
+        for index, number in enumerate(results[-1][1:]):
+            differences.append(number - results[-1][index])
+
+        results.append(differences)
+
+        if differences.count(0) == len(differences):
+            break
+
+    return results
+
+
+def extrapolate_previous_value(line):
+
+    tree = reduce(line)
+
+    tree.reverse()
+
+    extrapolated_values = []
+
+    for index, sequence in enumerate(list(tree[1:])):
+        new_value = sequence[0] - tree[index][0]
+        extrapolated_values.append(new_value)
+        tree[index + 1].insert(0, new_value)
+
+    return tree[-1][0]
+
+
+if __name__ == "__main__":
+
+    previous_values = [extrapolate_previous_value(line) for line in get_input('input.txt')]
+
+    print('Answer: ' + str(sum(previous_values)))
